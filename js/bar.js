099bb219b76883047c3cbe0c55347507ff72a3d0
sundlee <sundlee@gmail.com>
2019-08-31 13:57:29 +0900

    HTML&CSS 마스터 캠프 7주차 내용
    - grid, column(다단) 속성을 활용한 pinterest 레이아웃
    - HTML5 video 관련 요소
    - CSS Sprite 배경이미지
    - 반응형 콘텐츠(img, video)
    - iframe 요소를 활용한 반응형 콘텐츠

---
+++ js/bar.js
@@ -0,0 +1,3 @@
+var list = $('.member li:nth-child(n+2)');
+list.prepend($('<span class="bar" aria-hidden="true"></span>'));
+
--- js/webcafe.js
+++ js/webcafe.js
@@ -1,41 +1,36 @@
-// 뷰포트 사이즈를 최대 999px 설정하여 변수에 해당 값을 할당
 let viewport = window.matchMedia('(max-width:999px)');
-
-// resize 이벤트 발생 시 새로고침 실행
-$(window).resize(function () {
+$(window).resize(function(){
   location.reload();
 });
-
-if (viewport.matches) {
+if(viewport.matches){
   let nav = $('.navigation');
   let btn = $('.btn-menubar');
   let menuLists = $('.menu-list');
   let menuItems = $('.menu-item');
   let subMenus = $('.sub-menu');
-
-  // btn click 이벤트가 발생했을때
+  
+  // btn click 이벤트가 발생했을 때 
   // nav 요소에 is-act 클래스를 추가
-
-  btn.click(function () {
+  
+  btn.click(function(){
     nav.toggleClass('is-act');
-    if (nav.hasClass('is-act')) {
+    if(nav.hasClass('is-act')){
       btn.attr('aria-label', '메뉴 닫기');
       btn.attr('title', '메뉴 닫기');
-    } else {
+    }else{
       btn.attr('aria-label', '메뉴 열기');
       btn.attr('title', '메뉴 열기');
     }
   });
-
-  //.menu-item 클래스에 icon-plus 클래스 추가하기
+// .menu-item 클래스에 icon-plus 클래스를 추가하기
+  menuItems.addClass('icon-plus');
   menuItems.attr('aria-haspopup', 'true');
   menuItems.attr('aria-expanded', 'false');
   menuItems.attr('role', 'button');
   menuItems.attr('tabindex', '0');
-  menuItems.addClass('icon-plus');
 
-  menuItems.on('click keyup', function (e) {
-    if (e.type === 'click' || e.keyCode === 13) {
+  menuItems.on('click keyup', function(e){
+    if(e.type === 'click' || e.keyCode === 13){
       menuLists.removeClass('menu-act');
       $(this).parent().addClass('menu-act');
       menuItems.removeClass('icon-minus').addClass('icon-plus');
@@ -45,3 +40,5 @@ if (viewport.matches) {
     }
   });
 }
+
+
